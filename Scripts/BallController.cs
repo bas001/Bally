dc90bf7043773974b1e098792bb1d3cd523bc1fe
bseifert <bseifert@senacor.com>
2018-02-05 20:30:49 +0100

    creation of balls

--- Scripts/BallController.cs
+++ Scripts/BallController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BallController : MonoBehaviour {
 
 
-    bool gotHit = false;
+    bool getDestroyedOnNextHit = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,24 +13,36 @@ public class BallController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-      
+      if(InMotion())
+        {
+            getDestroyedOnNextHit = false;
+            GameController.SetIsAnyBallInMotion(true);
+        }
 	}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       
-        if(collision.collider.transform.tag == "blueBall" && gotHit)
+
+        if(collision.collider.transform.tag == gameObject.tag && getDestroyedOnNextHit)
         {
             Destroy(gameObject, 0.00000000001f);
         }
 
-        if (collision.collider.transform.tag == "blueBall" 
+        if (collision.collider.transform.tag == gameObject.tag
             || collision.collider.transform.tag == "Player")
         {
-            gotHit = true;
+            getDestroyedOnNextHit = true;
         }
     
-    
+    }
+
+    private bool InMotion()
+    {
+        if(GetComponent<Rigidbody2D>().velocity.x < 0.001 && GetComponent<Rigidbody2D>().velocity.y < 0.001)
+        {
+            return false;
+        }
+        return true;
     }
 
 }
--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -1,20 +1,131 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private static readonly int MAX_NUMBER_OF_TRYS = 50;
+    private static bool isAnyBallInMotion = false;
 
-        GameObject blueBall = (GameObject)Instantiate(Resources.Load("blueBall"));
-        GameObject redBall = (GameObject)Instantiate(Resources.Load("redBall"));
+    private System.Random rnd = new System.Random();
+    private Stopwatch sw = new Stopwatch();
+    private bool playing = true;
 
+    private GameObject table;
+    private CircleCollider2D ball;
 
+    public static void SetIsAnyBallInMotion(bool value)
+    {
+        isAnyBallInMotion = value;
+    }
+
+    // Use this for initialization
+    void Start () {
+        InstantiateGameObjects();
+        MinMaxVector.FindMinMax(table, ball);
+        sw.Start();
+    }
+
+    private void InstantiateGameObjects()
+    {
+        table = GameObject.FindWithTag("Table");
+        ball = GameObject.FindWithTag("circleCollider").GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+        // set to true by BallController
+        isAnyBallInMotion = false;
+    }
+
+    private void LateUpdate()
+    {
+        if(playing)
+        {
+            if (!isAnyBallInMotion && sw.ElapsedMilliseconds > 100)
+            {
+                CreateRandomBall();
+                sw.Reset();
+                sw.Start();
+            }
+        }
+      
+    }
+
+    private void CreateRandomBall()
+    {
+        Vector2 nextPosition;
+        bool colliding = true;
+        int counter = 0;
+        do
+        {
+            nextPosition = new Vector2(nextRandom(v2 => v2.x), nextRandom(v2 => v2.y));
+            colliding = Physics2D.OverlapCircle(nextPosition, ball.radius);
+            counter++;
+        } while (colliding && counter != MAX_NUMBER_OF_TRYS);
+
+        if(counter == MAX_NUMBER_OF_TRYS)
+        {
+            print("No space left on table");
+            playing = false;
+            return;
+        }
+
+        Instantiate(Resources.Load("blueBall"), nextPosition, Quaternion.identity);
+    }
+
+    private float nextRandom(Func<Vector2,float> GetPart)
+    {
+        int next = rnd.Next((int)(GetPart(MinMaxVector.min) * 100), (int)(GetPart(MinMaxVector.max) * 100));
+        return (float)next / 100;
+    }
+
+    
+    private static class MinMaxVector
+    {
+        public static Vector2 min;
+        public static Vector2 max;
+    
+        public static void FindMinMax(GameObject table, CircleCollider2D ball)
+        {
+            float minY = float.MaxValue;
+            float minX = float.MaxValue;
+            float maxY = float.MinValue;
+            float maxX = float.MinValue;
+
+            Vector2[] points = table.GetComponent<EdgeCollider2D>().points;
+            for (int i = 0; i < points.Length; i++)
+            {
+                float y = points[i].y * table.GetComponent<Transform>().localScale.y;
+                float x = points[i].x * table.GetComponent<Transform>().localScale.x;
+
+                if (y < minY)
+                {
+                    minY = y;
+                }
+                if (x < minX)
+                {
+                    minX = x;
+                }
+                if (y > maxY)
+                {
+                    maxY = y;
+                }
+                if (x > maxX)
+                {
+                    maxX = x;
+                }
+
+            }
+
+            min = new Vector2(minX + ball.radius, minY + ball.radius);
+            max = new Vector2(maxX - ball.radius, maxY - ball.radius);
+        }
+
+    }
+
+
 }
